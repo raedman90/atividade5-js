e9e0880cd1d98996d6b16ac7584f90422834f11f
Brunoklb <brunola.nogueira@gmail.com>
2021-05-12 17:01:21 -0300

    Adicionando a questão 1 e 2

---
+++ q2.js
@@ -0,0 +1,14 @@
+const usuarios = [
+    { name: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { name: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { name: 'Lucas', idade: 30, empresa: 'Facebook' },
+];
+
+const idade = usuarios.map(users => users.idade);
+
+const filter = usuarios.filter(users => users.empresa === 'Rocketseat' && users.idade > 18);
+
+const find = usuarios.find(users => users.empresa === 'Google');
+
+const calculo = usuarios.map(users => ({...users, idade : users.idade * 2})).filter(users => users.idade <= 50)
+console.log(calculo);
