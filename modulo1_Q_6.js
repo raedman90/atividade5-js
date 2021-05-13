c4a09889ba2c6679e2517106ad38b6084ae09199
moacirdavidag <moacirdavidag@gmail.com>
2021-05-13 10:21:53 -0300

    Adicionado scripts da questão 6 e 7 do módulo I

---
+++ modulo1_Q_6.js
@@ -0,0 +1,5 @@
+// Questão 6 TEMPLATE LITERALS
+const usuario = 'Diego';
+const idade = 23;
+
+console.log(`O usuário ${usuario} possui ${idade} anos`); 
---
+++ modulo1_Q_7.js
@@ -0,0 +1,11 @@
+const nome = 'Diego';
+const idade = 23;
+const cidade = 'Rio do Sul';
+
+const usuario = {
+  nome,
+  idade,
+  cidade
+}
+
+console.log(usuario);
