7c60bd59336e5611bd17836b362d9425607c86b2
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2021-05-13 16:41:59 -0300

    Adicionada a resposta do Módulo 2
    Adicionada a resposta do Módulo 2 da Atividade 5 de LSW.

---
+++ Modulo--2/modulo2.js
@@ -0,0 +1,10 @@
+//Quesito 1.1
+import { Usuarios as ClasseUsuario } from 'functions.js';
+ClasseUsuario.info();
+
+//Quesito 1.2
+import { idade } from 'functions.js';
+console.log(idade);
+
+//Quesito 1.3
+import defaultUsuario, { idade as IdadeUsuario } from 'funtions.js';
